0930bd25b8dff0257c4fdfc1184ac8ab1a4a44c6
Dimitris <toliasdimitris@gmail.com>
2022-12-07 20:16:19 +0200

    Added config file logic

--- directory_linker.py
+++ directory_linker.py
@@ -6,6 +6,7 @@ Created on Tue Dec  6 21:08:55 2022
 """
 
 import os
+import configparser
 
 class Linker:
     def __init__(self, project_path):
@@ -26,19 +27,45 @@ class Linker:
         return csv_dir
     
     def get_config_filepath(self):
-        return os.path.join(self.wdir, "config.txt")
+        return os.path.join(self.wdir, "config.ini")
     
     def get_building_id(self):
         config_path = self.get_config_filepath()
         
-        with open(config_path, "r") as file:        
-            return file.read()
+        config = configparser.ConfigParser()
+        config.read(config_path)
+        
+        return config["FIREBASE"]["buildingId"]
+
     
     def set_building_id(self, build_id):
         config_path = self.get_config_filepath()
         
-        if os.path.exists(config_path):
-            raise Exception("Building id is already set. If you wish to set a new id, delete the config.txt file first.")
+        ## Check if a building id already exists
+        config = configparser.ConfigParser()
+        config.read(config_path)
+        if config.has_option("FIREBASE", "buildingId"):
+            raise Exception("Building id is already set. If you wish to set a new id, remove the 'buildingId' line in the 'config.ini' file first.")
+        
+        ## Write the building id
+        config["FIREBASE"] = {}
+        config["FIREBASE"]["buildingId"] = build_id
         
         with open(config_path, 'w') as file:
-            file.write(build_id)
+            config.write(file)
+            
+    def get_vuforia_access_key(self):
+        config_path = self.get_config_filepath()
+        
+        config = configparser.ConfigParser()
+        config.read(config_path)
+        
+        return config["VUFORIA"]["accessKey"]
+    
+    def get_vuforia_secret_key(self):
+        config_path = self.get_config_filepath()
+        
+        config = configparser.ConfigParser()
+        config.read(config_path)
+        
+        return config["VUFORIA"]["secretKey"]
--- server.py
+++ server.py
@@ -6,41 +6,41 @@ This is a temporary script file.
 """
 
 import pyrebase
-import asyncio
+import configparser
 
-## Initialize firebase modules
+## Read config file
+config = configparser.ConfigParser()
+config.read("appConfig.ini")
+if not ("FIREBASE" in config):
+    raise Exception("Bad configuration: Missing application configuration file or missing 'FIREBASE' section in said file.")
 
-apiKey = "AIzaSyCsJYZrPmICYlOqEbGgsD0tAswaCJhTnMg"
-authDomain = "ar-visualguide.firebaseapp.com"
-databaseURL = "https://ar-visualguide-default-rtdb.europe-west1.firebasedatabase.app/"
-storageBucket = "ar-visualguide.appspot.com"
+fb_section = config["FIREBASE"]
 
-config = {
-  "apiKey": apiKey,
-  "authDomain": authDomain,
-  "databaseURL": databaseURL,
-  "storageBucket": storageBucket
+fb_config = {
+  "apiKey": fb_section["apiKey"],
+  "authDomain": fb_section["authDomain"],
+  "databaseURL": fb_section["databaseURL"],
+  "storageBucket": fb_section["storageBucket"]
 }
 
-firebase = pyrebase.initialize_app(config)
+## Initialize firebase modules
+firebase = pyrebase.initialize_app(fb_config)
 auth = firebase.auth()
 db = firebase.database()
 
-#authTask = asyncio.create_task(
-#    sign_in_with_email_and_password_async("toliasj@yahoo.gr", "password"))
+email = fb_section["username"]
+password = fb_section["password"]
+user = auth.sign_in_with_email_and_password(email, password)
 
-
-async def sign_in_with_email_and_password_async(email, password):
-    user = auth.sign_in_with_email_and_password(email, password)
-    return user
+print(user)
 
 def get_walls(building_id):
     ref = db.child("buildings").child(building_id).child("walls")
-    walls = ref.get().val()
+    walls = ref.get(user["idToken"]).val()
     return walls
 
 def get_building_ids():
-    build_ids = db.child("buildings").shallow().get().val()
+    build_ids = db.child("buildings").shallow().get(user["idToken"]).val()
     return build_ids
 
 def get_building_names():    
@@ -48,30 +48,36 @@ def get_building_names():
     names = {}
     for build_id in build_ids:
         ref = db.child("buildings").child(build_id).child("name")
-        name = ref.get().val()
+        name = ref.get(user["idToken"]).val()
         names[build_id] = name
     return names
         
 def get_building_data(build_id):
     ref = db.child("buildings").child(build_id)
-    return ref.get().val()
+    return ref.get(user["idToken"]).val()
 
 def set_walls(build_id, walls):
     ref = db.child("buildings").child(build_id).child("walls")
-    ref.set(walls)
+    ref.set(walls, user["idToken"])
 
 def set_name(build_id, name):
     ref = db.child("buildings").child(build_id).child("name")
-    ref.set(name)  
+    ref.set(name, user["idToken"])  
     
 def set_items_characteristics(build_id, data):
     ref = db.child("buildings").child(build_id).child("items_characteristics")
-    ref.set(data)
+    ref.set(data, user["idToken"])
     
 def set_items_coords(build_id, data):
     ref = db.child("buildings").child(build_id).child("items_coords")
-    ref.set(data)
+    ref.set(data, user["idToken"])
     
 def set_image_targets_coords(build_id, data):
     ref = db.child("buildings").child(build_id).child("image_targets_coords")
-    ref.set(data)
+    ref.set(data, user["idToken"])
+    
+def set_vuforia_keys(build_id, access_key, secret_key):
+    access_ref = db.child("buildings").child(build_id).child("vuforia_access_key")
+    access_ref.set(access_key, user["idToken"])
+    secret_ref = db.child("buildings").child(build_id).child("vuforia_secret_key")
+    secret_ref.set(secret_key, user["idToken"])
